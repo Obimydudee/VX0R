dc4cf29a6afcca5e141f42f0515b2e1a6d35c294
Obi1 <obi@vnight-studios.xyz>
2024-06-10 19:45:50 +0200

    Add files via upload

---
+++ VX0r/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace VX0r
+{
+    internal class Program
+    {
+        public static utils _utils = new utils();
+        static void Main(string[] args)
+        {
+            string[] array = new string[]
+            {
+            "Xor Encode", "Xor Decode", "Exit"
+            };
+
+            int num = 0;
+            while (true)
+            {
+                Console.Title = "| VX0R |";
+                Console.Clear();
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (j == num)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("> ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine(array[j]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                switch (System.Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (num > 0)
+                        {
+                            num--;
+                            System.Console.WriteLine(num);
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (num < array.Length - 1)
+                        {
+                            num++;
+                            System.Console.WriteLine(num);
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        if (num == array.Length - 1)
+                        {
+                            System.Console.WriteLine("Exiting program.");
+                            return;
+                        }
+                        switch (num)
+                        {
+                            case 0:
+                                XorEnc();
+                                break;
+                            case 1:
+                                XorDec();
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+
+        public static void XorDec()
+        {
+            Console.Clear();
+            Console.Write("Enter XorHexed string: ");
+            string inp = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Enter key: ");
+            string key = Console.ReadLine();
+            Console.WriteLine($"\n{_utils.Dec(inp, key)}");
+            Console.ReadKey();
+        }
+
+        public static void XorEnc()
+        {
+            Console.Clear();
+            Console.Write("Enter string: ");
+            string inp = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Enter key: ");
+            string key = Console.ReadLine();
+            Console.WriteLine($"\n{_utils.Enc(inp, key)}");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ VX0r/utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace VX0r
+{
+    internal class utils
+    {
+        public utils() { }
+
+        public string FromHex(string hex)
+        {
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            byte[] anon = raw;
+            string slin = Encoding.ASCII.GetString(anon, 0, anon.Length);
+
+            return slin;
+        }
+
+        public string Enc(string plaintext, string pad)
+        {
+            char[] data = plaintext.ToCharArray();
+            char[] key = pad.ToCharArray();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (char)(data[i] ^ key[i % key.Length]);
+            }
+            return bytes(new string(data));
+        }
+
+        public string Dec(string enctext, string pad)
+        {
+            var data = FromHex(enctext);
+            char[] key = pad.ToCharArray();
+            return Encoding.UTF8.GetString(data.Select((b, i) => (byte)(b ^ key[i % key.Length])).ToArray());
+        }
+
+        public string bytes(string inse)
+        {
+            string decString = inse;
+            byte[] bytes = Encoding.Default.GetBytes(decString);
+            string hexString = BitConverter.ToString(bytes);
+            hexString = hexString.Replace("-", "");
+            return hexString.ToLower();
+        }
+    }
+}
